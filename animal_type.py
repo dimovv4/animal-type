8d5ceb12d66e6421d4260ef423a551e3d6437d89
Georgi Dimov <147717567+dimovv4@users.noreply.github.com>
2023-11-11 13:28:32 +0200

    Create animal_type.py

---
+++ animal_type.py
@@ -0,0 +1,13 @@
+animal = input()
+animal_output = ''
+if animal == 'dog':
+    animal_output = 'mammal'
+elif animal == 'crocodile':
+    animal_output = 'reptile'
+elif animal == 'tortoise':
+    animal_output = 'reptile'
+elif animal == 'snake':
+    animal_output = 'reptile'
+else:
+    print("unknown")
+print(animal_output)
